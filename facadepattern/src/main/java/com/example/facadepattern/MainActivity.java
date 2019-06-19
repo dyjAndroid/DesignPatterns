a180c8923d7811b55925874490aa0c2a624148ad
Sunday <ytdyj2017@sina.com>
2019-06-19 10:44:36 +0800

    外观模式

--- facadepattern/src/main/java/com/example/facadepattern/MainActivity.java
+++ facadepattern/src/main/java/com/example/facadepattern/MainActivity.java
@@ -1,10 +1,10 @@
-package com.example.adapterpattern;
+package com.example.facadepattern;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 /**
- * 适配器模式：将一个类的接口，转换成客户期望的另一个接口。适配器让原本接口不兼容的类可以合作无间。
+ * 外观模式：提供了一个统一的接口，用来访问子系统中的一群接口。外观定义了一个高层接口，让子系统更容易使用。
  */
 public class MainActivity extends AppCompatActivity {
 
